97094b18034456d51b278040a9159dda42c2b9a7
Aditya Ganti <adi10rox@gmail.com>
2023-11-19 15:44:18 -0500

    add header navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavigationHeader from './components/NavigationHeader';
+
+const links = [
+  { label: 'Home', href: '/' },
+  { label: 'About', href: '/about' },
+  { label: 'Login', href: '/login' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationHeader />
     </div>
   );
 }
