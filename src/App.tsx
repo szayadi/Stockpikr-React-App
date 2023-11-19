0c84b9069e7cf6183b9b0018e531312da62a5756
Aditya Ganti <adi10rox@gmail.com>
2023-11-19 15:45:18 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import NavigationHeader from './components/NavigationHeader';
 
-const links = [
-  { label: 'Home', href: '/' },
-  { label: 'About', href: '/about' },
-  { label: 'Login', href: '/login' },
-];
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">     
       <NavigationHeader />
     </div>
   );
