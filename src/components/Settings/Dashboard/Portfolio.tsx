30ee78c7d6a60e6f33be0489b747b4a95d0cbfa7
Annapurna Mandalika <amandalika19@outlook.com>
2023-12-30 16:58:16 -0800

    updated json data, and imported into portfolio

--- src/components/Settings/Dashboard/Portfolio.tsx
+++ src/components/Settings/Dashboard/Portfolio.tsx
@@ -7,87 +7,116 @@ import {
   TableContainer,
   TableRow
 } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function createPortfolioData(
-  symbol: string,
-  name: string,
-  quantity: number,
-  avgPrice: number,
-  marketCap?: string,
-  balance?: number,
-  price?: number,
-  sevenDayChange?: number,
-  thirtyDayChange?: number,
-  oneYearChange?: number,
-  todayChangePercent?: number
-) {
+interface StockData {
+  symbol: string;
+  name: string;
+  quantity: number;
+  avgPrice: number;
+  balance: number;
+  price: number;
+  changesPercentage: number;
+  thirtyDayChange: number;
+  oneYearChange: number;
+  todayChangePercent: number;
+  dayLow: number;
+  dayHigh: number;
+  yearHigh: number;
+  yearLow: number;
+  marketCap: number;
+  open: number;
+  previousClose: number;
+}
+
+function createPortfolioDataFromJson(symbol: string, entry: any): StockData {
   return {
-    symbol,
-    name,
-    quantity,
-    avgPrice,
-    marketCap: marketCap || 0,
-    balance: balance || 0,
-    price: price || 0,
-    sevenDayChange: sevenDayChange || 0,
-    thirtyDayChange: thirtyDayChange || 0,
-    oneYearChange: oneYearChange || 0,
-    todayChangePercent: todayChangePercent || 0,
+    symbol: entry.symbol || symbol,
+    name: entry.name || '',
+    quantity: entry.quantity || 0,
+    avgPrice: entry.avgPrice || 0,
+    marketCap: entry.marketCap || 0,
+    balance: entry.balance || 0,
+    price: entry.price || 0,
+    changesPercentage: entry.changesPercentage || 0,
+    thirtyDayChange: entry.thirtyDayChange || 0,
+    oneYearChange: entry.oneYearChange || 0,
+    todayChangePercent: entry.todayChangePercent || 0,
+    dayLow: entry.dayLow || 0,
+    dayHigh: entry.dayHigh || 0,
+    yearHigh: entry.yearHigh || 0,
+    yearLow: entry.yearLow || 0,
+    open: entry.open || 0,
+    previousClose: entry.previousClose || 0,
   };
 }
 
-const portfolioData = [
-  createPortfolioData('AAPL', 'Apple Inc.', 12, 195.0, '2.5T', 50000, 155.0, -2.5, 5.0, 15.0, 1.2),
-  createPortfolioData('GOOGL', 'Alphabet Inc.', 5, 115.0, '1.8T', 75000, 118.0, 3.0, -1.5, 10.0, -0.5),
-  createPortfolioData('AMZN', 'Amazon.com Inc.', 20, 150, '1.7T', 60000, 164.0, 8.0, 2.0, 25.0, 2.5),
-  createPortfolioData('MSFT', 'Microsoft Corp.', 93, 345, '2.0T', 70000, 325.0, -1.0, 4.0, 18.0, -0.8),
-  createPortfolioData('NFLX', 'Netflix Inc.', 84, 287, '12T', 45000, 0, 5.0, -3.0, 8.0, 1.0),
-  createPortfolioData('TSLA', 'Tesla Inc.', 29, 1234, '0.4T', 0, 0, 2.0, 7.0, 30.0, 0.5),
-];
-
-
 const Portfolio: React.FC = () => {
-  
+  const [portfolioData, setPortfolioData] = useState<StockData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const json = require('../../../assets/mock_data.json');
+
+        const stockData : StockData[] = json.stocks || [];
+        console.log('Stocks:', stockData);
+
+        const stockEntries = stockData.map((stock: StockData) => {
+          return createPortfolioDataFromJson(stock.symbol, stock);
+        });
+        
+        setPortfolioData(stockEntries);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Box sx={{ borderRadius: '16', padding: '0 10px', marginBottom: '10px' }}>
-    <Paper elevation={1} variant="elevation" square={false} sx={{ display: 'flex', flexDirection: 'row',  backgroundColor: 'white' }}>
-      <TableContainer component={Paper} sx={{}}>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell>Symbol</TableCell>
-              <TableCell align="right">Name</TableCell>
-              <TableCell align="right">Quantity</TableCell>
-              <TableCell align="right">Average Price</TableCell>
-              <TableCell align="right">Market Cap</TableCell>
-              <TableCell align="right">Balance</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">7-Day Change</TableCell>
-              <TableCell align="right">30-Day Change</TableCell>
-              <TableCell align="right">1-Year Change</TableCell>
-              <TableCell align="right">Today's Change Percent</TableCell>
-            </TableRow>
-            {portfolioData.map((row) => (
-              <TableRow key={row.symbol} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row" sx={{ }}>{row.symbol}</TableCell>
-                <TableCell align="right">{row.quantity}</TableCell>
-                <TableCell align="right">{row.name}</TableCell>
-                <TableCell align="right">{row.avgPrice}</TableCell>
-                <TableCell align="right">{row.marketCap}</TableCell>
-                <TableCell align="right">{row.balance}</TableCell>
-                <TableCell align="right">{row.price}</TableCell>
-                <TableCell align="right" sx={{ color: (row.sevenDayChange > 0 ? 'green' : 'red') }}>{row.sevenDayChange}</TableCell>
-                <TableCell align="right" sx={{ color: row.thirtyDayChange > 0 ? 'green' : 'red' }}>{row.thirtyDayChange}</TableCell>
-                <TableCell align="right" sx={{ color: row.oneYearChange > 0 ? 'green' : 'red' }}>{row.oneYearChange}</TableCell>
-                <TableCell align="right" sx={{ color: row.todayChangePercent > 0 ? 'green' : 'red' }}>{row.todayChangePercent}</TableCell>
+      <Paper elevation={1} variant="elevation" square={false} sx={{ display: 'flex', flexDirection: 'row', backgroundColor: 'white' }}>
+        <TableContainer component={Paper} sx={{}}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Symbol</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Price</TableCell>
+                <TableCell align="right">Changes Percentage</TableCell>
+                <TableCell align="right">30-Day Change</TableCell>
+                <TableCell align="right">1-Year Change</TableCell>
+                <TableCell align="right">Today's Change Percent</TableCell>
+                <TableCell align="right">Day Low</TableCell>
+                <TableCell align="right">Day High</TableCell>
+                <TableCell align="right">Year High</TableCell>
+                <TableCell align="right">Year Low</TableCell>
+                <TableCell align="right">Previous Close</TableCell>
+                {/* Add other table header cells here */}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
-  </Box>
+              {portfolioData.map((row, index) => (
+                <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row" sx={{ }}>{row.symbol}</TableCell>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                  <TableCell align="right">{row.changesPercentage}</TableCell>
+                  <TableCell align="right">{row.thirtyDayChange}</TableCell>
+                  <TableCell align="right">{row.oneYearChange}</TableCell>
+                  <TableCell align="right">{row.todayChangePercent}</TableCell>
+                  <TableCell align="right">{row.dayLow}</TableCell>
+                  <TableCell align="right">{row.dayHigh}</TableCell>
+                  <TableCell align="right">{row.yearHigh}</TableCell>
+                  <TableCell align="right">{row.yearLow}</TableCell>
+                  <TableCell align="right">{row.previousClose}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
   );
 };
 
