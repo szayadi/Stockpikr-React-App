ac148c5ce04cdb71fe6be041ffaf3fe34f63c499
Nichlos Ho <hon1@seattleu.edu>
2024-02-21 13:50:04 -0800

    remove comments

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -65,14 +65,6 @@ const Dashboard: React.FC = () => {
     });
   }, []);
 
-  // const Item = styled(Paper)(({ theme }) => ({
-  //   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  //   ...theme.typography.body2,
-  //   padding: theme.spacing(1),
-  //   textAlign: 'center',
-  //   color: theme.palette.text.secondary
-  // }));
-
   return (
     <Grid container sx={{ marginTop: '50px' }}>
       <Grid item xs={12}>
