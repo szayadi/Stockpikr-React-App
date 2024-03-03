34a31a945c5a1c9658be7a9e4e74ed6b185213b2
Aditya Ganti <adi10rox@gmail.com>
2024-03-03 10:11:07 -0800

    Revert "updated port for Azure VM"
    This reverts commit a800f930d0405d07bbd573eece56d5dd1db19d01.

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -11,7 +11,7 @@ export class BaseApiService {
   public static get apiService(): AxiosInstance {
     if (BaseApiService._apiService == null) {
       BaseApiService._apiService = axios.create({
-        baseURL: process.env.REACT_APP_BACKEND_URL || '',
+        baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080',
         headers: {
           'Content-Type': 'application/json',
           'Cache-Control': 'no-cache'
